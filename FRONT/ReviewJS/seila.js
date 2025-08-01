fd5566c35be61e9f81386c49a62f41a16e531453
ramos-emily <ramos.emilyvi@gmail.com>
2025-08-01 14:45:10 -0300

    Atividade JS

---
+++ FRONT/ReviewJS/seila.js
@@ -0,0 +1,34 @@
+// //mostrar para o usuario
+// alert ("a")
+
+// //prompt -> input
+// prompt("What'is your favorite matter?")
+
+// //confirm
+// confirm("Do you like Java?")
+
+// //let: é uma declaração de variavel que muda de valor
+// //const: ele recebe um valor fixo
+// //var: primeira forma de fazer uma declaração de variavel, é legada mas nao indicada
+
+// //condicionais
+// let idade = Number(prompt("Qual a sua idade?"))
+// document.write(idade, "aaaaaaaaaaaaaaaa")
+
+// if(idade === 18){
+//     document.write("voce é velho")
+// }else{
+//     document.write("novinho")
+// }
+
+
+let Note1 = Number(prompt("Write the first note"))
+let Note2 = Number(prompt("Write the second note"))
+let Result = Note1+Note2
+
+if(Result < 5){
+    document.write("Burro do caralho")
+}else{
+    document.write("Passou fiiii!")
+}
+
