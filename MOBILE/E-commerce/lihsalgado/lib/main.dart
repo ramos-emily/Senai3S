73eb19846d21ea4fab7d0a226834fd4d7eb0619d
ramos-emily <ramos.emilyvi@gmail.com>
2025-10-08 16:57:11 -0300

    feat(mob): structure body

--- MOBILE/E-commerce/lihsalgado/lib/main.dart
+++ MOBILE/E-commerce/lihsalgado/lib/main.dart
@@ -11,104 +11,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
       home: Scaffold(
-         
-        
-        body: 
-            
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              spacing: 30, 
-              
+        body: Builder(
+          builder: (context) {
+            return Stack(
               children: [
-          Stack(children: 
+                
+                // Imagem de fundo
+                Container(
+                  width: double.infinity,
+                  height: 500,
+                  decoration: const BoxDecoration(                
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/banner.jpg") ,
+                      fit: BoxFit.cover,
+                      
+                    ),
+                  ),
+                ),
 
-                    [
-                      
-                      
+                SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
                       Container(
-                        width: double.infinity,
-                        height: MediaQuery.of(context).size.width * 0.1,
-                        color: const Color.fromARGB(0, 253, 252, 252),
-                        child:
-                        Center(child: Stack(
-                          children: [
-                                  Image.asset('assets/images/logo.png', width: double.infinity,height: 300,),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                    
-                                    Icon(Icons.search, size: 30.0,),
-                                    Container(child: Row(children: [
-                                      Icon(Icons.person, size: 30.0,),
-                                      Icon(Icons.store, size: 30.0,)
-                                    ],),)
-                                  ],)
-                          ],
-                        ))
+                        padding: const EdgeInsets.all(30),                       
                         
-                      )
-
-
-                          
-                          ] ),
-
-                Row(
-                  spacing: 30, 
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 150,
-                      height: 150,
-                      child: Center(child: Text("1", style: TextStyle(color: Colors.white),),),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Color.fromARGB(255, 0, 0, 0),
+                        width: double.infinity,
+                        child: 
+                            Row(      
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,                              
+                              children: [
+                                
+                                const Icon(Icons.search, size: 40.0, color:  Color.fromARGB(255, 168, 85, 16)),
+                                Image.asset(
+                              'assets/images/logo.png',
+                              width: 120,
+                              height: 80,
+                              
+                            ),
+                            Icon(Icons.person, size: 40.0, color:   Color.fromARGB(255, 168, 85, 16)),
+                              ],
+                            )                        
                       ),
-                    ),
-                    Container(
-                      width: 150,
-                      height: 150,
-                      child: Center(child: Text("2", style: TextStyle(color: Colors.white),),),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Color.fromARGB(255, 0, 0, 0),
-                      
-                      ),
-                    ),
-                  ],
-                ),                
-                Row(
-                  spacing: 30, 
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 150,
-                      height: 150,
-                      child: Center(child: Text("3", style: TextStyle(color: Colors.white),),),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Color.fromARGB(255, 0, 0, 0),
-                      ),
-                    ),
-                    Container(
-                      width: 150,
-                      height: 150,
-                      child: Center(child: Text("4", style: TextStyle(color: Colors.white),),),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Color.fromARGB(255, 0, 0, 0),),),
-                  ],
+                    ],
+                  ),
+
                   
                 ),
-                              Image.asset(
-                  'assets/images/logo.png',
-                  width: 200,
-                )
-              
               ],
-            ),
+            );
+          }, 
+        ),
       ),
     );
   }
